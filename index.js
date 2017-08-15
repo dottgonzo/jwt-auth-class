834d0d1165acc02750ee0517e95c8b6ed5c18a13
Dario Caruso <dev@dariocaruso.info>
2017-08-15 22:32:29 +0200

    oki

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jwt = require("jsonwebtoken");
+var Auth = (function () {
+    function Auth(secret, options) {
+        this.options = {
+            ignoreExpiration: false,
+            ignoreNotBefore: false
+        };
+        this.secret = secret;
+        if (options)
+            this.options = options;
+    }
+    Auth.prototype.verify = function (token) {
+        var that = this;
+        var t = false;
+        var o = {
+            ignoreExpiration: that.options.ignoreExpiration,
+            ignoreNotBefore: that.options.ignoreNotBefore
+        };
+        try {
+            t = jwt.verify(token, that.secret, o);
+        }
+        catch (err) {
+            return false;
+        }
+        return t;
+    };
+    Auth.prototype.sign = function (object) {
+        var that = this;
+        if (that.options.ignoreExpiration) {
+            jwt.sign(object, that.secret, { noTimestamp: true });
+        }
+        else {
+            jwt.sign(object);
+        }
+    };
+    return Auth;
+}());
+exports.Auth = Auth;
---
+++ index.ts
@@ -0,0 +1,40 @@
+import * as jwt from "jsonwebtoken"
+
+export interface IJwtAuthOptions {
+    ignoreExpiration: boolean
+    ignoreNotBefore: boolean
+}
+export class Auth {
+    secret: string
+    options: IJwtAuthOptions = {
+        ignoreExpiration: false,
+        ignoreNotBefore: false
+    }
+    constructor(secret, options: IJwtAuthOptions) {
+        this.secret = secret
+        if (options) this.options = options
+    }
+    verify(token): any {
+        const that = this
+        let t = false
+        const o = {
+            ignoreExpiration: that.options.ignoreExpiration,
+            ignoreNotBefore: that.options.ignoreNotBefore
+        }
+        try {
+            t = jwt.verify(token, that.secret, o)
+        } catch (err) {
+            return false
+        }
+        return t
+    }
+    sign(object) {
+        const that = this
+        if (that.options.ignoreExpiration) {
+            jwt.sign(object, that.secret, { noTimestamp: true })
+        } else {
+            jwt.sign(object)
+        }
+
+    }
+}
